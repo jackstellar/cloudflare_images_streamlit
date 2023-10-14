2eaf6711031f9d040d3029e18be0eee2754e7db7
jackstellar <147904216+jackstellar@users.noreply.github.com>
2023-10-14 07:57:46 +0100

    Add files via upload
    V0.1

--- cloudflare_images.py
+++ cloudflare_images.py
@@ -4,12 +4,80 @@ import json
 
 st.set_page_config(layout="wide")
 
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
+
+st.markdown(hide_img_fs, unsafe_allow_html=True)
+
 # Cloudflare API details
-API_TOKEN = "ogLef6Hjkaep_GUBJekTY7Re6QYw-_p5otHqyC_C"  # Replace with your Cloudflare API token
-ACCOUNT_ID = "a9eb62809a84081e8eb1cc2d527231c8"  # Replace with your Cloudflare account ID
+API_TOKEN = "ogLef6Hjkaep_GUBJekTY7Re6QYw-_p5otHqyC_C"
+ACCOUNT_ID = "a9eb62809a84081e8eb1cc2d527231c8"
 
 # Cloudflare Images API endpoint
 API_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/images/v1"
+
+# Streamlit app layout
+def main():
+    st.header("Upload to Cloudflare Images")
+
+    # Image uploader
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        metadata = st.text_input("Image Metadata (optional)", "")
+        require_signed_urls = st.checkbox("Require Signed URLs")
+
+        if st.button("Upload Image"):
+            response = upload_image(uploaded_image, metadata, require_signed_urls)
+            response_data = response.json()
+
+            if response_data.get("success", False):
+                st.success("Image uploaded successfully!")
+                st.experimental_rerun()
+            else:
+                st.write(response.json())
+                st.error("Failed to upload image. Please check the Cloudflare API response.")
+
+    st.header("Cloudflare Images Gallery")
+    # Pagination
+    pag_col1, pag_col2 = st.columns(2)
+    with pag_col1:
+        page_number = st.number_input("Page number", value=1, min_value=1, step=1)
+    with pag_col2:
+        per_page = st.number_input("Images per page", value=12, min_value=10, max_value=10000, step=10)
+
+    # Fetch images using Cloudflare API
+    response = fetch_images(page_number, per_page)
+    images = response.get("result", {}).get("images", [])
+
+    if not images:
+        st.write("No images found.")
+    else:
+        st.write("Displaying {} images:".format(len(images)))
+
+        # Create a grid layout with st.columns and display images with st.image()
+        num_cols = 4
+        col_index = 0
+        for image in images:
+            if col_index % num_cols == 0:
+                cols = st.columns(num_cols)
+            col = cols[col_index % num_cols]
+
+            image_url_cf = "https://imagedelivery.net/-SEsWSAsICljQF4lX8NqTA/"
+            image_url = f"{image_url_cf}{image.get('id')}/original"
+
+            if image_url is not None:
+                col.image(image_url, caption=image.get('filename', 'N/A'), use_column_width=True)
+            else:
+                # Display the error message using st.error() and remove the third argument
+                col.error(f"Image details not found for {image.get('id', 'N/A')}")
+
+            col_index += 1
+
 
 # Fetch images using Cloudflare API
 def fetch_images(page_number, per_page):
@@ -28,50 +96,21 @@ def fetch_images(page_number, per_page):
 
     return data
 
-# Display images in the Streamlit app
-# Display images in the Streamlit app
-def display_images(images):
-    num_columns = 3  # Define the number of columns in the grid layout
-    col_list = st.columns(num_columns)
-    for image in images:
-        try:
-            if 'url' in image:
-                col_idx = 0
-                for col in col_list:
-                    with col:
-                        st.image(image['url'], use_column_width=True)
-                        col_idx += 1
-                        if col_idx >= num_columns:
-                            break
-            else:
-                st.write(f"Image details not found for ID: {image.get('id', 'N/A')}")
-        except Exception as e:
-            st.write(f"Error displaying image with ID {image.get('id', 'N/A')}: {e}")
-
-
-# Streamlit app layout
-def main():
-    # Initialize page_number and per_page
-    page_number = 1
-    per_page = 12
-
-    st.header("Upload to Cloudflare Images")
-
-    # ... (Your previous code)
-
-    st.header("Cloudflare Images Gallery")
-    # Fetch images using Cloudflare API
-    response = fetch_images(page_number, per_page)
-    images = response.get("result", {}).get("images", [])
-
-    if not images:
-        st.write("No images found.")
-    else:
-        st.write("Displaying {} images:".format(len(images)))
-        display_images(images)
-
 # Upload image using Cloudflare API
-# ... (Your previous code)
+def upload_image(image, metadata, require_signed_urls):
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+
+    data = {
+        "metadata": json.dumps(metadata),
+        "requireSignedURLs": require_signed_urls,
+        "id": image.name
+
+    }
+
+    response = requests.post(API_BASE_URL, headers=headers, data=data, files={"file": (image.name, image)})
+    return response
 
 if __name__ == "__main__":
     main()
